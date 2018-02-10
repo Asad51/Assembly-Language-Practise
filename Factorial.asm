ba8e87b7c9da1374f70a3bd60c8756172db517a4
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-02-10 22:40:00 +0600

    An assembly program to find factorial of given numnber from 0 to 3

---
+++ Factorial.asm
@@ -0,0 +1,50 @@
+TITLE Series Summation
+.MODEL SMALL
+.STACK 100H
+.DATA
+    CR EQU 0DH
+    LF EQU 0AH
+    MSG1 DB 'Enter a number : ',CR,LF,'$'
+    MSG2 DB CR,LF,'Factorial : ','$'
+    NUM DB 100 DUP(?)
+    SUM DB 00H
+
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H
+
+    MOV AH,1
+    INT 21H
+
+    SUB AL,30H
+    MOV BL,AL
+    MOV AL,01H
+
+FACTORIAL:
+    CMP BL,01H
+    JLE EXIT
+    MUL BL
+    DEC BL
+    JMP FACTORIAL
+
+EXIT:
+    MOV BL,AL
+    LEA DX,MSG2
+    MOV AH,9
+    INT 21H
+
+    ADD BL,30H
+    MOV AH,2
+    MOV DL,BL
+    INT 21H
+
+    MOV AH,4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
